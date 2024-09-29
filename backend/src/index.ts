6a2ef01351cd74d50e76eddf87d5dd4c5a2504f2
Harizibam7 <harizibamv@gmail.com>
2024-09-29 12:09:25 +0530

    Backend with Jwt with hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,56 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {decode,jwt,sign, verify} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:String
+  }
+}>()
 
-app.get('/', (c) => {
+app.get('/', (c:any) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signup',(c) => {
-  return c.text('This is from signup')
+app.post('/api/v1/user/signup',async(c:any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl:  c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password
+    }
+  });
+
+  const token = await sign({id:user.id},c.env.JWT_SECRET);
+  
+  return c.json({
+    jwt:token
+  })
 })
 
-app.post('/api/v1/user/signin',(c)=>{
+app.post('/api/v1/user/signin',(c:any)=>{
   return c.text('This is from signin')
 })
 
-app.post('/api/v1/blog',(c)=>{
+app.post('/api/v1/blog',(c:any)=>{
   return c.text('This from Blog')
 })
 
-app.put('/api/v1/blog',(c)=>{
+app.put('/api/v1/blog',(c:any)=>{
   return c.text('This is from put blog')
 })
 
-app.get('/api/v1/blog/:id',(c)=>{
+app.get('/api/v1/blog/:id',(c:any)=>{
   return c.text('This is from get method')
 })
 
-app.get('/api/v1/blog/bulk',(c) => {
+app.get('/api/v1/blog/bulk',(c:any) => {
   return c.text('This is from get ')
 })
 
